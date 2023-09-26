ac25f88bb0bef64a34bdfa545f14c31dc98046da
Martin Sirois <martin.sirois@statcan.gc.ca>
2023-09-26 19:03:21 -0400

    maj pour 2023-2024

--- My Project/Settings.Designer.vb
+++ My Project/Settings.Designer.vb
@@ -15,7 +15,7 @@ Option Explicit On
 Namespace My
     
     <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
-     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0"),  _
+     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0"),  _
      Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
     Partial Friend NotInheritable Class MySettings
         Inherits Global.System.Configuration.ApplicationSettingsBase
@@ -56,7 +56,7 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("F:\Users\MM\Documents\Git\SaisonEnCours\SaisonEnCours.mdb")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("C:\Git\SaisonEnCours\SaisonEnCours.mdb")>  _
         Public Property FichierBaseDeDonnées() As String
             Get
                 Return CType(Me("FichierBaseDeDonnées"),String)
@@ -68,7 +68,7 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("F:\Users\MM\Documents\Git\gehstatcan.github.io")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("C:\Git\gehstatcan.github.io")>  _
         Public Property RépertoiresSiteweb() As String
             Get
                 Return CType(Me("RépertoiresSiteweb"),String)
--- modDeclarations.vb
+++ modDeclarations.vb
@@ -5,7 +5,7 @@ Module modDeclarations
 	Public Const COL_HEADER_COLOR As String = "darkcyan"
 	Public Const F_DE_MATCH As String = "Questionnaires\match"
 	'UPGRADE_NOTE: YEAR was upgraded to YEAR_Renamed. Click for more: 'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="A9E4979A-37FA-4718-9994-97DD76ED70A7"'
-	Public Const YEAR_Renamed As String = "2022-2023"
+	Public Const YEAR_Renamed As String = "2023-2024"
 
 	'Public gcConn As ADODB.Connection
 	Public gcConn As OleDb.OleDbConnection
